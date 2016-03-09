3dae5145b0e6a00d74833c921981f136f384bbef
Sergey Semipiadniy <sergei.semipyadniy@propeople.com.ua>
2016-03-09 16:14:26 +0200

    FK-341 | Select all feature on affiliated domain content page.

--- modules/custom/domain_extention/js/domain_extention.js
+++ modules/custom/domain_extention/js/domain_extention.js
@@ -4,11 +4,58 @@
  */
 (function ($) {
 
-  Drupal.behaviors.domain_extention = {
+  Drupal.behaviors.domainExtention = {
     attach: function(context, settings) {
-      $('#select-all', context).each(function() {
-        console.log('works');
-        console.log(settings);
+
+      var checkboxes, lastChecked;
+      var table = $('th.select-all', context).closest('table');
+
+      var updateSelectAll = function (state) {
+        // Update table's select-all checkbox (and sticky header's if available).
+        $(table).prev('table.sticky-header').andSelf().find('th.select-all input:checkbox').each(function() {
+          this.checked = state;
+        });
+      };
+
+      var selectContent = $('tbody', table).prepend('<tr class="select-content">' + '<td colspan="16">select all</td>' + '</tr>').find('.select-content');
+      selectContent.addClass('hide-row');
+
+      $('th.select-all', table)
+        .prepend($('<input type="checkbox" class="domain-extention-select-all" />'))
+        .click(function (event) {
+          if ($(event.target).is('input:checkbox')) {
+            console.log(event);
+
+            if (event.target.checked) {
+              selectContent.removeClass('hide-row');
+            }
+            else {
+              selectContent.addClass('hide-row');
+            }
+
+            // Loop through all checkboxes and set their state to the select all checkbox' state.
+            checkboxes.each(function () {
+              this.checked = event.target.checked;
+            });
+            // Update the title and the state of the check all box.
+            updateSelectAll(event.target.checked);
+          }
+        });
+
+      checkboxes = $('td input:checkbox:enabled', table).click(function (e) {
+        // Either add or remove the selected class based on the state of the check all checkbox.
+        $(this).closest('tr').toggleClass('selected', this.checked);
+
+        // If all checkboxes are checked, make sure the select-all one is checked too, otherwise keep unchecked.
+        updateSelectAll((checkboxes.length == $(checkboxes).filter(':checked').length));
+
+        // Keep track of the last checked checkbox.
+        lastChecked = e.target;
+      });
+
+      $('#filtered-count', context).each(function() {
+        //console.log('works');
+        console.log(settings.domainExtention.filteredCount);
       });
     }
   }
