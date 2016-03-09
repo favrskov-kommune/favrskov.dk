b10397eb4ea413cec478d7467f5a907e678e9656
sergey <semipiadniy@i.ua>
2016-03-10 00:19:16 +0200

    FK-341 | feature

--- modules/custom/domain_extention/js/domain_extention.js
+++ modules/custom/domain_extention/js/domain_extention.js
@@ -7,33 +7,53 @@
   Drupal.behaviors.domainExtention = {
     attach: function(context, settings) {
 
+      var filteredCount = settings.domainExtention.filteredCount;
+      var onPageArgs  = {'@defaultCount' : '50'},
+          onTableArgs = {'@filteredCount' : filteredCount};
+
+      var text5 = Drupal.t('Select all @filteredCount nodes in this table.', onTableArgs);
       var checkboxes, lastChecked;
       var maxNodesPerPage = 50;
-      var showSelectAllRow = (parseInt(settings.domainExtention.filteredCount) > maxNodesPerPage) ? true : false;
-      var table = $('th.select-all', context).closest('table');
+      var showSelectAllRow = (parseInt(filteredCount) > maxNodesPerPage) ? true : false;
+      var $table = $('th.select-all', context).closest('table');
+      /*input2 = '<span class="vbo-table-this-page" style="display: none;">' +
+              Drupal.t('Selected <strong>%filteredCount</strong> in this page. &nbsp', args) +
+              '<input class="vbo-table-select-all-pages form-submit" ' +
+              'type="submit" ' +
+              'name="op" ' +
+              'value="' + Drupal.t('Select all %defaultCount nodes in this table.', args) + '">' +
+              '</span>';*/
+      var $nodesOnPage = $('<span>');
+      $nodesOnPage.addClass('nodes-on-page')
+          .append(Drupal.t('Selected <strong>@defaultCount</strong> in this page.', onPageArgs))
+          .append('<input type="submit" />')
+          .find('input')
+          .attr('class', 'form-submit')
+          .attr('name', 'op')
+          .attr('value', text5);
 
       var updateSelectAll = function (state) {
         // Update table's select-all checkbox (and sticky header's if available).
-        $(table).prev('table.sticky-header').andSelf().find('th.select-all input:checkbox').each(function() {
+        $table.prev('table.sticky-header').andSelf().find('th.select-all input:checkbox').each(function() {
           this.checked = state;
         });
       };
 
-      var selectContent = $('tbody', table).prepend('<tr class="select-content">' + '<td colspan="16">select all</td>' + '</tr>').find('.select-content');
-      selectContent.addClass('hide-row');
+      var $tbody = $('tbody', $table).prepend('<tr></tr>');
+      var $selectContentRow = $('tr:first', $tbody).addClass('select-content hide-row');
+      $selectContentRow.append('<td></td>').find('td:first').attr('colspan', 16)
+        .append($nodesOnPage);
 
-      $('th.select-all', table)
-        .prepend($('<input type="checkbox" class="domain-extention-select-all" />'))
+      $('th.select-all', $table)
+        .prepend('<input type="checkbox" class="domain-extention-select-all" />')
         .click(function (event) {
           if ($(event.target).is('input:checkbox')) {
-
-            console.log(event);
-
+            //console.log(event);
             if (event.target.checked && showSelectAllRow) {
-              selectContent.removeClass('hide-row');
+              $selectContentRow.removeClass('hide-row');
             }
             else {
-              selectContent.addClass('hide-row');
+              $selectContentRow.addClass('hide-row');
             }
 
             // Loop through all checkboxes and set their state to the select all checkbox' state.
@@ -46,7 +66,7 @@
         });
 
 
-      checkboxes = $('td input:checkbox:enabled', table).click(function (e) {
+      checkboxes = $('td input:checkbox:enabled', $table).click(function (e) {
         // If all checkboxes are checked, make sure the select-all one is checked too, otherwise keep unchecked.
         updateSelectAll((checkboxes.length == $(checkboxes).filter(':checked').length));
 
