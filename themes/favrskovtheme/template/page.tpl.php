ae97f7c659cca2456acf780e606ea8235e078502
Sergey Semipiadniy <sergei.semipyadniy@propeople.com.ua>
2016-06-30 14:53:37 +0300

    FK-357 | Added minimap.js via hook_init.

--- themes/favrskovtheme/template/page.tpl.php
+++ themes/favrskovtheme/template/page.tpl.php
@@ -1,12 +1,1 @@
-<?php
-  // Added minimap.js only to article and news pages.
-  if ($node && (in_array($node->type, ['article', 'news']))) {
-    $minimap = [
-      '#type' => 'markup',
-      '#markup' => "<script type='text/javascript' src='https://webkort.favrskov.dk/clientapi/minimap2/2.4.x/minimap.js' charset='windows-1252'></script>",
-    ];
-    drupal_add_html_head($minimap, 'minimap');
-  }
-?>
-
 <?php print render($page['content']); ?>
