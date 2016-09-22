3afcb8e202dae29eb338a9fef97ba255ae2833d7
Sergei Semipyadniy <sergei.semipyadniy@ffwagency.com>
2016-09-22 17:52:12 +0300

    FK-366 | Re-patched core..

--- drupal/sites/default/default.settings.php
+++ drupal/sites/default/default.settings.php
@@ -616,3 +616,5 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash sign to enable.
  */
 # $conf['theme_debug'] = TRUE;
+
+$base_url = "http" . (isset($_SERVER["HTTPS"]) && strtolower($_SERVER["HTTPS"]) == "on" ? "s" : "") . "://" . $_SERVER["HTTP_HOST"];
