6fa66a2b5bc06730377940821ba112d358e8e98e
Sergei Semipyadniy <sergei.semipyadniy@ffwagency.com>
2017-02-28 20:00:08 +0200

    FKM-14 | Wrong display of PDF files.

---
+++ drupal/profiles/favrskov/themes/favrskovtheme/template/file-entity/file--document--wysiwyg.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Suggested template for file_type document and wysiwyg view_mode.
+ *
+ * @see template_preprocess_file_entity()
+ * @see file_entity.tpl.php
+ *
+ */
+?>
+
+<?php print render($content); ?>
