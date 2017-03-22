4b465ea0cd2faab0777915fb7af1e08780a9cb1c
Temoor Gilmutdinov <temoor.gilmutdinov@propeople.com.ua>
2017-03-22 13:35:35 +0200

    Updates to reinstall and components

--- drupal/reinstall.sh
+++ drupal/reinstall.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-if [ "$1" = "--windows" ]; then
-    time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local --extra-vars "is_windows=true"
+if [ "$IS_DOCKER" = "1" ]; then
+    time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local --extra-vars "pp_environment=docker env_domain=docksal mysql_host=db solr_host=solr solr_url='solr/collection1' memcache_server='memcached:11211'"
 else
-    time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local
+    if [ "$1" = "--windows" ]; then
+        time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local --extra-vars "is_windows=true"
+    else
+        time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local
+    fi
 fi
