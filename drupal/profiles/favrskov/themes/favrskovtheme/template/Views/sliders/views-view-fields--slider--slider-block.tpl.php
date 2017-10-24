52910bc4a4ee67d2fe61621cc6490febf803c4b7
--get <andrew_fall@ukr.net>
2017-10-24 10:46:25 +0300

    FKM-32. Feature implementation

--- drupal/profiles/favrskov/themes/favrskovtheme/template/Views/sliders/views-view-fields--slider--slider-block.tpl.php
+++ drupal/profiles/favrskov/themes/favrskovtheme/template/Views/sliders/views-view-fields--slider--slider-block.tpl.php
@@ -1,8 +1,8 @@
 <?php if (!empty($fields['field_external_link']->content)): ?>
-  <?php print l($fields['field_image']->content, $fields['field_external_link']->content, array(
+  <?php print l($fields['field_billede_iframe']->content, $fields['field_external_link']->content, array(
     'html' => TRUE,
     'attributes' => array('target' => $fields['field_external_link_1']->content)
   )) ?>
 <?php else: ?>
-  <?php print $fields['field_image']->content ?>
+  <?php print $fields['field_billede_iframe']->content ?>
 <?php endif ?>
