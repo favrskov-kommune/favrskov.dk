f0a818a4a0b546367457110ab25c7f04fed54add
Dion Sune Jensen <dsj@novicell.dk>
2020-06-23 18:08:20 +0200

    Fix curl calls

--- .docker/deploy/rollback/rollback.sh
+++ .docker/deploy/rollback/rollback.sh
@@ -9,7 +9,7 @@
 # $7 = deployed site URL
 # $8 = deployed site domain
 
-curl -X POST -H 'Content-type: application/json' --data '{"attachments": [{"title": "Favrskov.dk", "title_link": "'$7'", "text": ":bangbang: Release of `'$2'` to <'$7'|'$8'> failed, starting rollback.","color": "CF423F","mrkdwn_in": ["title","text"]}]}' $6
+curl -s -X POST -H 'Content-type: application/json' --data "{\"attachments\": [{\"title\": \"Favrskov.dk\", \"title_link\": \"$7\", \"text\": \":bangbang: Release of `$2` to <$7|$8> failed, starting rollback.\",\"color\": \"CF423F\",\"mrkdwn_in\": [\"title\",\"text\"]}]}" $6
 
 cd /mnt/data/docker/favrskov/
 # Local .env
@@ -17,5 +17,5 @@ if [ -f .env ]; then
     # Load Environment Variables
     export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
     # Do rollback here
-    curl -X POST -H 'Content-type: application/json' --data '{"attachments": [{"title": "Favrskov.dk", "title_link": "'$7'", "text": ":white_check_mark: Release of `'$2'` to <'$7'|'$8'> successfully rolled back.","color": "4AB441","mrkdwn_in": ["title","text"]}]}' $6
+    curl -s -X POST -H 'Content-type: application/json' --data "{\"attachments\": [{\"title\": \"Favrskov.dk\", \"title_link\": \"$7\", \"text\": \":white_check_mark: Release of `$2` to <$7|$8> successfully rolled back.\",\"color\": \"4AB441\",\"mrkdwn_in\": [\"title\",\"text\"]}]}" $6
 fi
