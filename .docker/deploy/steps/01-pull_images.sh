f13ecc799d6365b8edb292ac583e3142d24bbeb9
Dion Sune Jensen <dsj@novicell.dk>
2020-06-23 14:56:14 +0200

    Fix docker command.
    the quiet parameter is not available on the server.

--- .docker/deploy/steps/01-pull_images.sh
+++ .docker/deploy/steps/01-pull_images.sh
@@ -3,6 +3,6 @@
 cd /mnt/data/docker/favrskov/
 sed -i "s/AZURE_DEPLOY_STATUS=.*/AZURE_DEPLOY_STATUS=STARTED/" .env
 sed -i "s/AZURE_DEPLOY_STAGE=.*/AZURE_DEPLOY_STAGE=IMAGE_PULL_START/" .env
-docker pull -q favrskov.azurecr.io/favrskov-apache:$1
-docker pull -q favrskov.azurecr.io/favrskov-php:$1
+docker pull favrskov.azurecr.io/favrskov-apache:$1
+docker pull favrskov.azurecr.io/favrskov-php:$1
 sed -i "s/AZURE_DEPLOY_STAGE=.*/AZURE_DEPLOY_STAGE=IMAGE_PULL_STOP/" .env
