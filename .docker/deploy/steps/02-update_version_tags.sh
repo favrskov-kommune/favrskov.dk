82fe1bf434cf8542098226fe9ae4ff89f0e39b56
Dion Sune Jensen <dsj@novicell.dk>
2020-06-23 12:13:39 +0200

    Update scripts

--- .docker/deploy/steps/02-update_version_tags.sh
+++ .docker/deploy/steps/02-update_version_tags.sh
@@ -2,7 +2,11 @@
 
 cd /mnt/data/docker/favrskov/
 sed -i "s/AZURE_DEPLOY_STAGE=.*/AZURE_DEPLOY_STAGE=TAGGING_START/" .env
-while read line; do export $line; done < .env
-sed -i "s/PREVIOUS_TAG=.*/PREVIOUS_TAG=$RELEASE_TAG/" .env
-sed -i "s/RELEASE_TAG=.*/RELEASE_TAG=$1/" .env
-sed -i "s/AZURE_DEPLOY_STAGE=.*/AZURE_DEPLOY_STAGE=TAGGING_STOP/" .env
+# Local .env
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+    sed -i "s/PREVIOUS_TAG=.*/PREVIOUS_TAG=$RELEASE_TAG/" .env
+    sed -i "s/RELEASE_TAG=.*/RELEASE_TAG=$1/" .env
+    sed -i "s/AZURE_DEPLOY_STAGE=.*/AZURE_DEPLOY_STAGE=TAGGING_STOP/" .env
+fi
