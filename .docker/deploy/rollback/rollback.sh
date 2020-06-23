27a6d514e8873864fdfc7c4ca7dcf6a39e5fe6c0
Dion Sune Jensen <dsj@novicell.dk>
2020-06-23 17:08:08 +0200

    Add argument docblock

--- .docker/deploy/rollback/rollback.sh
+++ .docker/deploy/rollback/rollback.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Variables:
+# $1 = release tag
+# $2 = branch name (for feature/some-feature the name would be "some-feature")
+# $3 = webhook for #notif-drupal-deploy channel
+# $4 = webhook for #notif-deployments channel
+# $5 = webhook for #notif-php-devops channel
+# $6 = webhook for #notif-php-test channel
 
 cd /mnt/data/docker/favrskov/
 # Local .env
@@ -6,4 +13,6 @@ if [ -f .env ]; then
     # Load Environment Variables
     export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
     # Do rollback here
+
+    echo "notif-php-test: $6"
 fi
--- .docker/deploy/steps/07-maintenance_off.sh
+++ .docker/deploy/steps/07-maintenance_off.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Variables:
+# $1 = release tag
+# $2 = branch name (for feature/some-feature the name would be "some-feature")
+# $3 = webhook for #notif-drupal-deploy channel
+# $4 = webhook for #notif-deployments channel
+# $5 = webhook for #notif-php-devops channel
+# $6 = webhook for #notif-php-test channel
 
 cd /mnt/data/docker/favrskov/
 sed -i "s/AZURE_DEPLOY_STAGE=.*/AZURE_DEPLOY_STAGE=MAINTENANCE_OFF_START/" .env
