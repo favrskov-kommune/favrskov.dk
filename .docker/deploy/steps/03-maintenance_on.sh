7cff72d511518d5da2a4f91fff441bc3b3e43086
Dion Sune Jensen <dsj@novicell.dk>
2020-06-24 11:02:45 +0200

    Remove unnecessary logos.

--- .docker/deploy/steps/03-maintenance_on.sh
+++ .docker/deploy/steps/03-maintenance_on.sh
@@ -12,11 +12,11 @@
 cd /mnt/data/docker/favrskov/
 sed -i "s/AZURE_DEPLOY_STAGE=.*/AZURE_DEPLOY_STAGE=MAINTENANCE_ON_START/" .env
 
-curl -s -X POST -H 'Content-type: application/json' --data "{\"attachments\": [{\"title\": \":favrskov-logo: Favrskov.dk\", \"title_link\": \"$7\", \"text\": \":azure-logo: Release of branch \`$2\` to <$7|$8> started...\",\"color\": \"E6E6E6\",\"mrkdwn_in\": [\"title\",\"text\"]}]}" $3
+curl -s -X POST -H 'Content-type: application/json' --data "{\"attachments\": [{\"title\": \":favrskov-logo: Favrskov.dk\", \"title_link\": \"$7\", \"text\": \"Release of branch \`$2\` to <$7|$8> started...\",\"color\": \"E6E6E6\",\"mrkdwn_in\": [\"title\",\"text\"]}]}" $3
 
 # If master branch also notify in #notif-deployments
 if [ $2 = "master" ]; then
-    curl -s -X POST -H 'Content-type: application/json' --data "{\"attachments\": [{\"title\": \":favrskov-logo: Favrskov.dk\", \"title_link\": \"$7\", \"text\": \":azure-logo: Release of branch \`$2\` to <$7|$8> started...\",\"color\": \"E6E6E6\",\"mrkdwn_in\": [\"title\",\"text\"]}]}" $4
+    curl -s -X POST -H 'Content-type: application/json' --data "{\"attachments\": [{\"title\": \":favrskov-logo: Favrskov.dk\", \"title_link\": \"$7\", \"text\": \"Release of branch \`$2\` to <$7|$8> started...\",\"color\": \"E6E6E6\",\"mrkdwn_in\": [\"title\",\"text\"]}]}" $4
 fi
 
 docker-compose exec php bash -c "cd sites/default/ && drush vset maintenance_mode 1"
