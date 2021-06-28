f0b59ecc38347c91ccbc6751fb83fe539c9d6b5f
Søren Due Bonde <sdb@novicell.dk>
2021-06-28 07:33:20 +0200

    FAVRDK-34: Fix read more buttons on window resize.

--- webroot/sites/favrskov.dk/themes/custom/favrskov/build-assets/src/03-organisms/appetiser/appetiser.js
+++ webroot/sites/favrskov.dk/themes/custom/favrskov/build-assets/src/03-organisms/appetiser/appetiser.js
@@ -1,5 +1,6 @@
 function appLoop() {
-  const apps = document.querySelectorAll('.js-appetiser--small:not(.show-more)');
+  // const apps = document.querySelectorAll('.js-appetiser--small:not(.show-more)');
+  const apps = document.querySelectorAll('.js-appetiser--small');
   if (apps.length === 0) {
     return;
   }
@@ -7,28 +8,40 @@ function appLoop() {
   for (let i = 0; i < apps.length; i += 1) {
     const appContWrap = apps[i].querySelector('.appetiser__content-wrapper');
     const appContent = apps[i].querySelector('.appetiser__content');
-    const appExpHeight = appContent.offsetHeight + 30;
     const appReadMore = apps[i].querySelector('.appetiser__read-more');
 
-    if (appContent.offsetHeight > appContWrap.offsetHeight) {
-      apps[i].classList.add('show-more');
-
-      appReadMore.addEventListener('click', (event) => {
-        event.preventDefault();
-        if (appContWrap.classList.contains('expanded')) {
-          appContWrap.classList.remove('expanded');
-          appContWrap.removeAttribute('style');
-        } else {
-          appContWrap.classList.add('expanded');
-          appContWrap.setAttribute('style', `max-height:${appExpHeight}px; height:${appExpHeight}px`);
-        }
-      }, false);
+    if (!appContWrap.classList.contains('expanded')) {
+      if (appContent.offsetHeight > appContWrap.offsetHeight) {
+        apps[i].classList.remove('show-more');
+        apps[i].classList.add('show-more');
+      } else {
+        apps[i].classList.remove('show-more');
+      }
     }
+
+    appReadMore.addEventListener('click', (event) => {
+      event.preventDefault();
+      if (appContWrap.classList.contains('expanded')) {
+        appContWrap.classList.remove('expanded');
+      } else {
+        appContWrap.classList.add('expanded');
+      }
+    });
   }
 }
 
-Drupal.behaviors.appetiser = {
-  attach(context) {
-    appLoop();
-  },
-};
+function debounce(func, timeout) {
+  let timer;
+  return function foobar(event) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(func, timeout, event);
+  };
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  appLoop();
+});
+
+window.addEventListener('resize', () => {
+  debounce(appLoop(), 150);
+});
