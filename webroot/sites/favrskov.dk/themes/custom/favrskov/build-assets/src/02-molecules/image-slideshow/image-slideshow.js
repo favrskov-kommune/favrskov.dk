73099cca38941e2d0a31c306d4825b1a19af6f23
Kasper Basse <kdb@novicell.dk>
2021-08-03 15:09:11 +0200

    FAVRDK-40: Change layout for slideshow, so it matches design

--- webroot/sites/favrskov.dk/themes/custom/favrskov/build-assets/src/02-molecules/image-slideshow/image-slideshow.js
+++ webroot/sites/favrskov.dk/themes/custom/favrskov/build-assets/src/02-molecules/image-slideshow/image-slideshow.js
@@ -1,6 +1,4 @@
 import Flickity from 'flickity';
-import FlickityFade from 'flickity-fade';
-import FlickityAsNavFor from 'flickity-as-nav-for';
 import 'flickity/css/flickity.css';
 
 Drupal.behaviors.imageSlideshow = {
@@ -21,20 +19,12 @@ Drupal.behaviors.imageSlideshow = {
           // options
           cellAlign: 'left',
           contain: true,
-          pageDots: false,
+          pageDots: true,
           prevNextButtons: false,
           fade: true,
           wrapAround: true,
           autoPlay: current.dataset.autoplay === 'true' ? 3000 : '',
           cellSelector: '.image-slideshow-slide',
-        });
-
-        const flktyNavigation = new Flickity(navigation, {
-          // options
-          contain: true,
-          pageDots: false,
-          prevNextButtons: false,
-          asNavFor: slideshow,
         });
       });
     }
