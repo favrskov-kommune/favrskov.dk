fd4bb92cb7fe3e443b63f509a05c38c05776bb14
AndersHusted <adh@novicell.dk>
2024-04-25 12:45:23 +0200

    Accordion arrows shown wrong bugfix

--- webroot/sites/favrskov.dk/themes/custom/favrskov/build-assets/src/03-organisms/accordion/accordion.js
+++ webroot/sites/favrskov.dk/themes/custom/favrskov/build-assets/src/03-organisms/accordion/accordion.js
@@ -26,12 +26,11 @@ Drupal.behaviors.accordion = {
         <div :aria-expanded="isOpen ? 'true' : 'false'" :aria-controls="'accordion-content-' + id" :class="{'active': isOpen}" class="accordion-item__headline" @click="toggleAccordionItem">
           <h3 class="accordion-item__title">{{ title }}</h3>
           <div class="accordion-item__icon"></div>
-        </button>
+        </div>
         <div class="accordion-item__content" :aria-hidden="!isOpen ? 'true' : 'false'" :id="'accordion-content-' + id" :class="{'active': isOpen}">
           <div class="accordion-item__text">
             <slot/>
           </div>
-        </div>
         </div>
       `,
     };
